0b6c294e4d9e1f72fc428ddb0653148e5c36e9b9
Radu Ioan Fericean <radu@fericean.ro>
2022-06-29 14:27:57 +0300

    With basic auth

--- auth_download.py
+++ auth_download.py
@@ -1,44 +1,78 @@
 import http.server
-import socketserver
-from urllib.parse import urlparse
-from urllib.parse import parse_qs
+import cgi
+import base64
+import json
+from urllib.parse import urlparse, parse_qs
 
-class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        # Extract query param
-        query_components = parse_qs(urlparse(self.path).query)
-        user, passw = '', ''
-        if 'user' in query_components:
-            user = query_components["user"][0]
-        if 'pass' in query_components:
-            passw = query_components["pass"][0]
-        if user != 'adi' or passw != 'test':
-            self.send_response(403)
-            self.end_headers()
-            return
 
-        # Sending an '200 OK' response
+class CustomServerHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_HEAD(self):
         self.send_response(200)
-
-        # Setting the headers
-        self.send_header("Content-type", "application/pdf")
-        self.send_header('Content-Disposition', 'attachment; filename="response.pdf"')
-
-        # Whenever using 'send_header', you also have to call 'end_headers'
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
 
-        # Read the file and send the contents
-        with open('response.pdf', 'rb') as file:
-            self.wfile.write(file.read())
+    def do_AUTHHEAD(self):
+        self.send_response(401)
+        self.send_header(
+            'WWW-Authenticate', 'Basic realm="Demo Realm"')
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
 
-        return
+    def do_GET(self):
+        key = self.server.get_auth_key()
 
-# Create an object of the above class
-handler_object = MyHttpRequestHandler
+        ''' Present frontpage with user authentication. '''
+        if self.headers.get('Authorization') == None:
+            self.do_AUTHHEAD()
 
-PORT = 8080
-my_server = socketserver.TCPServer(("", PORT), handler_object)
+            response = {
+                'success': False,
+                'error': 'No auth header received'
+            }
 
-print("Http Server Serving at port", PORT)
-# Star the server
-my_server.serve_forever()
+            self.wfile.write(bytes(json.dumps(response), 'utf-8'))
+
+        elif self.headers.get('Authorization') == 'Basic ' + str(key):
+              self.send_response(200)
+
+              # Setting the headers
+              self.send_header("Content-type", "application/pdf")
+              self.send_header('Content-Disposition', 'attachment; filename="response.pdf"')
+
+              # Whenever using 'send_header', you also have to call 'end_headers'
+              self.end_headers()
+
+              # Read the file and send the contents
+              with open('response.pdf', 'rb') as file:
+                  self.wfile.write(file.read())
+        else:
+            self.do_AUTHHEAD()
+
+            response = {
+                'success': False,
+                'error': 'Invalid credentials'
+            }
+
+            self.wfile.write(bytes(json.dumps(response), 'utf-8'))
+
+
+
+class CustomHTTPServer(http.server.HTTPServer):
+    key = ''
+
+    def __init__(self, address, handlerClass=CustomServerHandler):
+        super().__init__(address, handlerClass)
+
+    def set_auth(self, username, password):
+        self.key = base64.b64encode(
+            bytes('%s:%s' % (username, password), 'utf-8')).decode('ascii')
+
+    def get_auth_key(self):
+        return self.key
+
+
+if __name__ == '__main__':
+    server = CustomHTTPServer(('', 8000))
+    server.set_auth('adi', 'test')
+    server.serve_forever()
